895c4a903f5feb3ae12155cf4f10ee5324dcec6d
dmitryzhigar <dmitry.zhigar@gmail.com>
2019-02-05 21:56:24 +0300

    Added Game Exception

---
+++ src/com/zhigar/gameException/GameException.java
@@ -0,0 +1,10 @@
+package com.zhigar.gameException;
+
+import java.io.IOException;
+
+public class GameException extends IOException {
+
+    public GameException(String message) {
+        super(message);
+    }
+}
